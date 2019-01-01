8253cca5fcefe9d4369c87dca728e17f9a79a82d
Robert Bednarczyk <rob.bednarczyk@gmail.com>
2019-01-01 16:22:08 +0100

    Add tests for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,12 +4,16 @@ const request = require("supertest");
 const {app} = require("./../server.js");
 const {Todo} = require("./../models/todo.js");
 
+const todos = [{text:"First test todo"}, {text:"Second test todo"}];
+
 // add a testing lifecycle method
 // before each - run code before every test case
 beforeEach((done) => {
     // empty the db before every test case - request
-    Todo.remove({}).then(() => done());
-})
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
+});
 
 describe("POST /todos", () => {
     // specify done as it is an async request
@@ -28,7 +32,7 @@ describe("POST /todos", () => {
                     // wrap up the tests printing the error to the screen
                     return done(err);
                 }
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -48,9 +52,21 @@ describe("POST /todos", () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             })
     })
+});
+
+describe("GET /todos", () => {
+    it("should get all todos", (done) => {
+        request(app)
+            .get("/todos")
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
 })
