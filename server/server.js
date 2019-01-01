96c94279c1e84ef4bc508d964aa9d201b92d6637
Robert Bednarczyk <rob.bednarczyk@gmail.com>
2019-01-01 15:15:34 +0100

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,16 @@ app.post("/todos", (req, res) => {
     });
 });
 
+// configure get route
+app.get("/todos", (req, res) => {
+    Todo.find().then((todos) => {
+        // sending back an object instead of an array - more flexible
+        res.send({todos})
+    }, (err) => {
+        res.status(400).send(err);
+    })
+});
+
 app.listen(3000, () => {
     console.log("Started on port 3000");
 });
